05d6509193b6dd4fd3aed1173930540b590f02f5
Fabian Lien <fabian.lien@gmail.com>
2022-05-11 16:35:34 +0000

    fix view to render necessary data

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.Main.as_view(), name='home')
+    path('', views.MainPage.as_view(), name='home'),
 ]
--- WebApp/views.py
+++ WebApp/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.views import generic, View
-from .models import Flavour
+from .models import Flavour, About
 
 
 # Create your views here.
-class Main(generic.ListView):
-    model = Flavour
-    queryset = Flavour.objects.order_by('category', 'name')
-    template_name = 'index.html'
+class MainPage(View):
+
+    def get(self, request, *args, **kwargs):
+        about = About.objects.get(id=1)
+        flavour_list = Flavour.objects.order_by('category', 'name')
+        return render(request, 'index.html', {
+            "about": about,
+            "flavour_list": flavour_list
+            })
