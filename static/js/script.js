f1aa29c53ea0ca19afd80b301fbef5f7e604a805
Fabian Lien <fabian.lien@gmail.com>
2022-05-14 08:15:15 +0000

    add reservation section with collapse element to nybrogatan23

--- static/js/script.js
+++ static/js/script.js
@@ -7,12 +7,14 @@ $(document).ready(function () {
     };
     const map = new google.maps.Map(document.getElementById("google-map"), {
       zoom: 14,
+      mapId: 'c3bf27aa22c010f7',
       center: Nybrogatan23,
     });
     const marker = new google.maps.Marker({
       position: Nybrogatan23,
       map: map,
     });
+  
   }
 
   window.initMap = initMap;
