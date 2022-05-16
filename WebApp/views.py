a1e52fb9e9300a5d4ffdb40c78b197829211ffc8
Fabian Lien <fabian.lien@gmail.com>
2022-05-16 15:49:26 +0000

    add contact info from contacts model to nybrogatan23 template

--- WebApp/views.py
+++ WebApp/views.py
@@ -29,9 +29,11 @@ class Nybrogatan23(View):
         pdf_menu = MenuPDF.objects.get(id=1)
         text_content = Nybro23Text.objects.get(id=1)
         image_list = Nybro23Image.objects.order_by('order_ranking', 'name')
+        contact_list = Contact.objects.order_by('order_ranking', 'name')
         return render(request, 'nybrogatan23.html', {
             'pdf_menu': pdf_menu,
             'text_content': text_content,
-            'images': image_list
+            'images': image_list,
+            'contacts': contact_list
         })
 
